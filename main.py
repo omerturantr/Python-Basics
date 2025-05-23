8e1401ae8a9ede20631358b801a8dabb0bd84764
omerturan <omerturanpersonal@gmail.com>
2025-05-24 02:26:51 +0300

    Add main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+print("Hello, Python!")
+print("I'm learning Python!")
+print("My name is Ömer.")
+
+name = "Ömer"
+age = 21
+height = 1.85
+is_student = True
+
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Is he Student?:", is_student)
